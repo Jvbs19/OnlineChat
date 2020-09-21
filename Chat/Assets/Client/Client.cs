795e1dec268739f8ef099351c012867ab38e4e45
João Vitor Batista dos Santos <joaovbs0504@gmail.com>
2020-09-21 14:49:22 -0300

    Initial commit

---
+++ Chat/Assets/Client/Client.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    public GameObject LoginPanel;
+    public GameObject ChatDeFala;
+    public GameObject MensagemPrefab;
+
+    string nome = "";
+
+    private bool socketReady = false;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+    public void ConnectToServer()
+    {
+
+        if (socketReady)
+        {
+            return;
+        }
+        //Se estiver conectado aciona com valores default
+        string host = "127.0.0.1";
+        int port = 6331;
+
+        //Reajusta valores se algo for digitado
+        string h;
+        int p;
+        string n;
+
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+        {
+            host = h;
+        }
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+        {
+            port = p;
+        }
+        n = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (n != "")
+        {
+            nome = n;
+        }
+        // Criar o socket
+
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            LoginPanel.SetActive(false);
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //Estou recebendo mensagem
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                {
+                    OnIncomingData(data);
+                }
+            }
+        }
+    }
+    void OnIncomingData(string data)
+    {
+        if (data == "@NOMEE")
+        {
+            Send("#NOMEE|" + nome);
+            return;
+        }
+        GameObject ms = Instantiate(MensagemPrefab, ChatDeFala.transform) as GameObject;
+        ms.GetComponentInChildren<Text>().text = data;
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+        {
+            return;
+        }
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+}
---
+++ Chat/Assets/Script/Server.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.IO;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> clients; // Clientes conectados
+    private List<ServerClient> disconnectList; //Clientes desconectados
+
+    public int port = 6321;
+    private TcpListener server;
+    private bool serverStarted; // Saber se o server ja iniciou
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port); // Recebe conexão
+            server.Start(); // Começar conexão
+
+            StartListening();
+            serverStarted = true;
+            Debug.Log("Server Inicializado na porta " + port);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+        StartListening();
+
+        Broadcast("@NOMEE", new List<ServerClient>() { clients[clients.Count - 1] });
+        // Enviar uma mensagem a todos conectados, dizendo que alguem foi conectado;
+        // Broadcast(clients[clients.Count - 1].clientName + "Se conectou", clients);
+    }
+
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        if (data.Contains("#NOMEE"))
+        {
+            c.clientName = data.Split('|')[1];
+            Broadcast(c.clientName + " has connected", clients);
+            return;
+        }
+        Broadcast(c.clientName + " : " + data, clients);
+    }
+    private void Broadcast(string data, List<ServerClient> cl)
+    {
+        foreach (ServerClient c in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                Debug.Log("write error: " + e.Message + "To  client" + c.clientName);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!serverStarted) // Se o server nao tiver iniciado ele não vai executar o codigo
+        {
+            return;
+        }
+        foreach (ServerClient c in clients)// Para cada cliente conectado meu cliente "c"
+        {
+            // Perguntar se o cliente for conectado?
+            if (!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectList.Add(c);
+                continue;
+            }
+            // Se ele estiver conectado. Procure mensagens do cliente
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < disconnectList.Count -1; i++)
+        {
+            Broadcast(disconnectList[i].clientName +  " has disconnected", clients);
+            clients.Remove(disconnectList[i]);
+            disconnectList.RemoveAt(i);
+        }
+    }
+}
+
+public class ServerClient// Usado para definir quem esta conectando. visivel apenas no servidor
+{
+    public TcpClient tcp;
+    public string clientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+        clientName = "Guest";
+        tcp = clientSocket;
+    }
+   
+}
